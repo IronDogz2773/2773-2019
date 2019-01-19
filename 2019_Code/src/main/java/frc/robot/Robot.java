eac92e4ad3875c29f058ebc6d98b63a916ef2f28
Carson <31147266+treesniper12@users.noreply.github.com>
2019-01-19 12:34:22 -0600

    Intital Robot Commit

--- 2019_Code/src/main/java/frc/robot/Robot.java
+++ 2019_Code/src/main/java/frc/robot/Robot.java
@@ -5,93 +5,156 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
+package org.usfirst.frc.team2773.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
+ * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
+ * creating this project, you must also update the build.properties file in the
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+	public static final String kDefaultAuto = "Default";
+	public static final String kCustomAuto = "My Auto";
+	public String m_autoSelected;
+	public SendableChooser<String> m_chooser = new SendableChooser<>();
+	public Joystick joy;
+	public Joystick joy2;
+	
+	public double joyY;
+	public double jayz;
+	public double accel;
+	public double veloY;
+	public double veloZ;
+	
+	public Victor FL; //Finnifan_Leftson
+	public Victor BL; //Benjamen Leftson
+	public SpeedControllerGroup left = new SpeedControllerGroup(FL, BL);
+	
+	public Victor FR; //Felix RodrIgeese
+	public Victor BR; //Bobbert Raplhq.q
+	public SpeedControllerGroup right = new SpeedControllerGroup(FR, BR);
+	
+	public double maxSpeed;
+	
+	DifferentialDrive drive;
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
+		joy = new Joystick(1);
+		joy2 = new Joystick(2);
+		
+		joyY = 0;
+		jayz = 0;
+		
+		accel = 0.2;
+		
+		
+		
+		veloY = 0;
+		veloZ = 0;
+		
+		FL = new Victor(0);
+		FR = new Victor(0);
+		
+		BL = new Victor(0);
+		BR = new Victor(0);
+		
+		maxSpeed = 1;
+		
+		drive = new DifferentialDrive(left, right);
+		
+	}
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		m_autoSelected = m_chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
+		System.out.println("Auto selected: " + m_autoSelected);
+	}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		switch (m_autoSelected) {
+			case kCustomAuto:
+				// Put custom auto code here
+				break;
+			case kDefaultAuto:
+			default:
+				// Put default auto code here
+				break;
+		}
+	}
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() 
+	{
+		drive();
+	}
+	
+	public void drive()
+	{
+		maxSpeed = joy.getThrottle();
+		joyY = joy.getYChannel();
+		jayz = joy.getZChannel();
+		
+		if(Math.abs(joyY) > 0.1 && veloY < maxSpeed)
+			veloY += 0.2 * joyY * accel;
+		else if(Math.abs(joyY) <= 0.1 && Math.abs(veloY) > 0.1)
+			veloY -= 0.4 * accel * -veloY;
+		else
+			veloY = 0;
+		
+		if(veloY > maxSpeed)
+			veloY = maxSpeed;
+		
+		if(joyY > 0.1 || joyY < -0.1)
+			drive.tankDrive(veloY, veloY);
+		else if(Math.abs(jayz) > 0.1)
+			drive.tankDrive(-jayz * 0.5, jayz * 0.5);
+		else
+			drive.tankDrive(0, 0);
+	}
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-  }
-
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
-}
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
