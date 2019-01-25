faf59620bb3f300cc0cdf10362d13da4a916d878
treesniper12 <carsoncrull@gmail.com>
2019-01-24 18:54:45 -0600

    ASCII Dog

--- 2019_Code/src/main/java/frc/robot/Robot.java
+++ 2019_Code/src/main/java/frc/robot/Robot.java
@@ -186,4 +186,32 @@ public class Robot extends TimedRobot {
 	@Override
 	public void testPeriodic() {
 	}
-}                                  
+}                   
+
+                                                  /*:-                          
+                                                 /hdms                          
+                                                `ommmh.                          
+                                               `+mmmd/os+-`                      
+                                             `:ohmmmmdmmmmmhs/                    
+                                    -+oooo/+sdmmmmbmanmmmmmmmd``                  
+             ```...--..```        `+dmyyyhmmmmmmmmmneedsmmmmmmhyy+                
+      ``-/+syhhdmmmmmmmdhyso+++++sydms    hmmmmdmmmmtommmmmmmmmmmd`               
+     /ydmmmmmmmmmmmmmmmmmmmmmmy:./oo/`    .+so/./hmmmstopmmquichem+               
+      -odmmmmmmmjaredmmmmmmmmh`                  .dmmmmmmeatermmmdh               
+     .-`.+hmmmmmmismmmmmmmmmmms                  hmmmmmmmmmdhddy+-`               
+    `odmmdhshmmmmmthemmmmmmmmmd/     `+shys/      odmmmmdy+-` ``                   
+   `ymmmmmkhaimmmmmrealmmmmm:.      `hhailmmy      `-omm-                          
+   smsocksmismmmmmmmmvpmmmmm        :mmthemmm`       -mm:                          
+  `mmmmmmmmmbadmmmmmmmmmmmmmyo/`     ommorbd/     `/oymd.                          
+  -mmmsquidmmmmmmmmmmmmyommmmmms      `:+/:`     `dmds+.                           
+  `dmmmtastesmmmmmmmd/`hmmmmmd:                  smm/                             
+   ommmmmmgoodmmmmmdo` `oydmmmh-   ``        ``   /mmo                             
+   :malecmmmmmmmmdo.     ``-+syhsohddh-    +hddy+hmd+`                             
+   :mmisn'tmmmmd+.            ``.-:+hmy-.-:md+oyyy+`                               
+   ommmrealmmh+`                    /mmddddm/  ``                                  
+   `dmmmmmmmy/`                      -josephd                                       
+   +mmmmmds-`                         ydunlap                                       
+  .dmmmh+.                            .herwin`                                      
+ `ymmd/`                               .hmmmmo.                                     
+ommmmh/`                               .smmmmdy/.                                  
+`:::::::.             BEES              `:::::::*/
